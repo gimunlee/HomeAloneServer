c412d8dc7e0896e6c98e6fa6afeceb4a81bf3b31
Gimun Lee <gimunlee@kaist.ac.kr>
2016-07-15 21:00:45 +0900

    added get feed/

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express=require("express");
 var app=express();
 var body_parser=require("body-parser");
 
+var mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost/homealone");
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connectino error:'));
+
 app.use(body_parser.urlencoded({extended:true}));
 app.use(body_parser.json());
 
@@ -14,6 +21,10 @@ app.get("/",function(req, res) {
 	console.log("Hello");
 	res.write("Hello");
 	res.end();
+});
+
+db.once('open',function() {
+	conole.log("mongodb connected");
 });
 var port = 12016;
 app.listen(12016,function() {
--- feed.js
+++ feed.js
@@ -1,3 +1,4 @@
+// ec2-52-78-28-51.ap-northeast-2.compute.amazonaws.com:12016/feed/
 var pathPrefix="/feed";
 
 module.exports = function (app) {
@@ -18,7 +19,7 @@ module.exports = function (app) {
         res.writeHead(201);
 		res.write("post request done\n");
         res.write("class : " + req.query.class);
-        res.write("time : " + req.body.time);
+        res.write("time : " + req.query.time);
 		res.write("userid : " + req.body.userid);
 		res.write("title : " + req.body.title);
 		res.write("body : " + req.body.body);
@@ -26,5 +27,18 @@ module.exports = function (app) {
 			res.write("tags : " + req.body.tags[i]);
 		}
         res.end();
+    });
+    get("/",function(req, res) {
+        console.log("req.class, req.time logged in mongodb");
+        var myLogSchema=mongoose.Schema({
+            class: String
+            time: String
+        });
+        var MyLog = mongoose.model('MyLog',myLogSchema);
+        var first = new MyLog({class:req.query.class, time:req.query.time});
+        first.save(function(err, first) {
+            if(err) return console.error(err);
+            console.log(first.toString() + " sent");
+        });
     });
 }
