a7069ccd6f082a023c5a80ffd6542d27a6d9ac3b
Gimun Lee <gimunlee@kaist.ac.kr>
2016-07-15 20:06:30 +0900

    add get

--- feed.js
+++ feed.js
@@ -26,4 +26,26 @@ module.exports = function (app) {
 		}
         res.end();
     });
+    get("/post", function(req, res) {
+        console.log("post request received");
+		console.log("class : " + req.query.class);
+        console.log("time : " + req.query.time);
+		// console.log("userid : " + req.body.userid);
+		// console.log("title : " + req.body.title);
+		// console.log("body : " + req.body.body);
+		// for(var i=0;i<req.body.tags.length;i++) {
+		// 	console.log("tags : " + req.body.tags);
+		// }
+
+		res.write("post request done\n");
+        res.write("class : " + req.query.class);
+        res.write("time : " + req.body.time);
+		// res.write("userid : " + req.body.userid);
+		// res.write("title : " + req.body.title);
+		// res.write("body : " + req.body.body);
+		// for(var i=0;i<req.body.tags.length;i++) {
+		// 	res.write("tags : " + req.body.tags);
+		// }
+        res.end();
+    });
 }
